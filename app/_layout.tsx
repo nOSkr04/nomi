93506f26c1a90108d626e8f57b8e094ebf9009b8
nOSkr <tselmen.ihelp@gmail.com>
2024-07-19 15:08:34 +0800

    sync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,36 @@
+import { UserApi } from "@/apis";
+import { IAuth } from "@/interfaces/auth";
+import { IUser } from "@/interfaces/user";
 import { persistor, store } from "@/store";
+import { authLogout, authMe } from "@/store/auth-slice";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { Stack } from "expo-router";
+import { Stack, Navigator } from "expo-router";
 import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Provider } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { SWRConfig } from "swr";
+import useSWR, { SWRConfig } from "swr";
+
 const Layout = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state: { auth: IAuth }) => state.auth);
+  const { data: user } = useSWR<IUser>(
+    ["swr.user.me", token],
+    async () => {
+      return await UserApi.authMe();
+    },
+    {
+      onSuccess: (authData) => {
+        dispatch(authMe(authData));
+      },
+      onError: (err) => {
+        if (err.error.code === 401) {
+          dispatch(authLogout());
+        }
+      },
+    }
+  );
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
@@ -15,11 +38,11 @@ const Layout = () => {
           <GestureHandlerRootView style={styles.root}>
             <SafeAreaProvider>
               <BottomSheetModalProvider>
-                <Stack>
-                  <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Navigator>
                   <Stack.Screen
                     name="home/index"
                     options={{ headerShown: false }}
+                    redirect={!!user}
                   />
                   <Stack.Screen
                     name="home/image"
@@ -29,8 +52,14 @@ const Layout = () => {
                       presentation: "transparentModal",
                       fullScreenGestureEnabled: true,
                     }}
+                    redirect={!!user}
                   />
-                </Stack>
+                  <Stack.Screen
+                    name="index"
+                    options={{ headerShown: false }}
+                    redirect={!user}
+                  />
+                </Navigator>
               </BottomSheetModalProvider>
             </SafeAreaProvider>
           </GestureHandlerRootView>
